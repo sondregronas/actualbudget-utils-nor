8d912128f96f2b35b1620c0b790af51ee63a171e
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-10-15 16:44:04 +0200

    fix: broken scheduler

--- src/routine.py
+++ src/routine.py
@@ -4,9 +4,9 @@ import schedule
 
 from main import main
 
-schedule.every().hour.do(main(["-petu"]))
-schedule.every(4).hours.do(main(["-petub"]))
-schedule.every().sunday.at("00:05").do(main(["-ch"]))
+schedule.every().hour.do(main, ["-petu"])
+schedule.every(4).hours.do(main, ["-petub"])
+schedule.every().sunday.at("00:05").do(main, ["-ch"])
 
 while True:
     schedule.run_pending()

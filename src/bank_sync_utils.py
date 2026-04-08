1e34081d99c55cf2c4e320619a613e12f1055dfc
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2026-04-08 18:17:12 +0200

    fix: remove yesterdays items too, as they might only have the source accounts data

--- src/bank_sync_utils.py
+++ src/bank_sync_utils.py
@@ -1,19 +1,19 @@
 import logging
 
 from actual.database import Transactions
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from actual.utils.conversions import date_to_int
 
 
 def remove_uncleared_from_bank_synced(transactions: list[Transactions]):
-    today = date_to_int(datetime.now().date())
+    yesterday = date_to_int(datetime.now().date() - timedelta(days=1))
     for t in transactions:
         if t.account.bank_id:
             if not t.cleared:
                 t.delete()
                 logging.info(f"Deleted uncleared transaction: {t=}")
             # If the transaction was made TODAY, it might not have cleared properly yet, so we should also delete it
-            elif t.date == today:
+            elif t.date >= yesterday:
                 t.delete()
                 logging.info(f"Deleted fresh transaction that might not have cleared properly yet: {t=}")
